4331a2ca83d1c9d8c1ea9985eccf66b795ee7de4
djsz-12 <djsz-12@sr.dmcs.pl>
2017-05-23 20:48:07 +0200

    User is able to delete supplier

--- infracture/persistance/memory/supplier_repo.go
+++ infracture/persistance/memory/supplier_repo.go
@@ -3,6 +3,7 @@ package memory
 import (
 	"github.com/PiotrPrzybylak/koop-wooch/domain"
 	"strconv"
+	"errors"
 )
 
 func NewSupplierRepository() domain.SupplierRepository {
@@ -21,6 +22,24 @@ func (r *supplierRepository) Create(supplier domain.Supplier) (string, error) {
 	return supplier.ID, nil
 }
 
-func (s supplierRepository) ListAll() ([]domain.Supplier, error) {
-	return s.suppliers, nil
+func (r supplierRepository) ListAll() ([]domain.Supplier, error) {
+	return r.suppliers, nil
+}
+
+func (r *supplierRepository) Delete(id string) (error) {
+	var result int = -1
+	for index, value := range r.suppliers {
+		if value.ID == id {
+			result = index
+			break
+		}
+
+	}
+	if result != -1 {
+		r.suppliers = append(r.suppliers[:result], r.suppliers[result+1:]...)
+		return nil
+	} else {
+		return errors.New("There is no supplier with given id " + id)
+	}
+
 }
