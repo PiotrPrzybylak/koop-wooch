03b782a03ed06f38cde404553bec80d6d6d63d49
djsz-12 <djsz-12@sr.dmcs.pl>
2017-05-23 19:38:07 +0200

    Move suppliers to domain.

---
+++ infracture/persistance/memory/supplier_repo.go
@@ -0,0 +1,26 @@
+package memory
+
+import (
+	"github.com/PiotrPrzybylak/koop-wooch/domain"
+	"strconv"
+)
+
+func NewSupplierRepository() domain.SupplierRepository {
+	return &supplierRepository{}
+}
+
+type supplierRepository struct {
+	nextID   int
+	suppliers []domain.Supplier
+}
+
+func (r *supplierRepository) Create(supplier domain.Supplier) (string, error) {
+	r.nextID++
+	supplier.ID = strconv.Itoa(r.nextID)
+	r.suppliers = append(r.suppliers, supplier)
+	return supplier.ID, nil
+}
+
+func (s supplierRepository) ListAll() ([]domain.Supplier, error) {
+	return s.suppliers, nil
+}
